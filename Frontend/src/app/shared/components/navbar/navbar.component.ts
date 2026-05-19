b798d576a6ec69d05591a68976d167f65c20d640
Lucas Ribeiro <lu.ribeiro@duocuc.cl>
2026-05-18 23:07:32 -0400

    Update navbar.component.ts
    Se ajusto la navbar

--- Frontend/src/app/shared/components/navbar/navbar.component.ts
+++ Frontend/src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth.service';
 import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-navbar', standalone: true,
