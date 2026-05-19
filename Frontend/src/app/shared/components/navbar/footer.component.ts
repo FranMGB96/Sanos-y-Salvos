9a8a26fb4b834a626aef12f555453c4fd644cedf
Lucas Ribeiro <lu.ribeiro@duocuc.cl>
2026-05-18 23:08:03 -0400

    Update footer.component.ts
    Se añadieron las redes sociales al footer

--- Frontend/src/app/shared/components/navbar/footer.component.ts
+++ Frontend/src/app/shared/components/navbar/footer.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router'; // Importante para la navegación
 
+
+
 @Component({
   selector: 'app-footer',
   standalone: true,
